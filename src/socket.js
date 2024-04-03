22c421614e2353cff4a6154e3fbd50ade3c5cf67
unknown <eddhall0821@gmail.com>
2024-04-03 11:30:08 +0900

    build

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:4000";
+const ip = "http://129.153.88.212:4000";
+const local = "http://localhost:4000";
+
+const URL = process.env.NODE_ENV === "production" ? ip : local;
 
 export const socket = io(URL);
